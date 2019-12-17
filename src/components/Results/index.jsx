d07427702551b04ffcd02f46ee97c6c9d503c3d9
DanBurbach <dburbach1982@gmail.com>
2019-12-16 23:36:51 -0800

    begin framework for Results form

--- src/components/Results/index.jsx
+++ src/components/Results/index.jsx
@@ -1,22 +1,52 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-class Results extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ""
-    };
-  }
+import posed from "react-pose";
+// import { keyframes } from "popmotion";
 
+const Box = posed.div({
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 }
+});
 
+const Results = ({ currentResultsArray }) => {
+  return (
+    <Box
+      className="box"
+      pose={currentResultsArray.length > 0 ? "visible" : "hidden"}
+    >
+      <em>List of messages</em>
+      {currentResultsArray.map(messages => {
+        return (
+          <li key={messages.id} className="card">
+            <h3>{messages.body}</h3>
+            <p>
+              Name: {messages.user.name} | Species: {messages.species}
+            </p>
+            <p>
+              Likes: {messages.likes.total} | Gender: {messages.gender}
+            </p>
+            <p>Origin: {messages.origin.name}</p>
+            <p>Location: {messages.location.name}</p>
+          </li>
+        );
+      })}
+    </Box>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <div> Results </div>
-      </div>
-    );
-  }
-}
+Results.propTypes = {
+  currentResultsArray: PropTypes.arrayOf(Object),
 
-export default Results;
+  name: PropTypes.string,
+  dispatch: PropTypes.func
+};
+
+const mapStateToProps = state => {
+  return {
+    currentResultsArray: state.currentResultsArray
+  };
+};
+
+export default connect(mapStateToProps)(Results);
