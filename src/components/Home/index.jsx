5f31dbe9abe19fa9dffd5414a03f564ed655ea89
DanBurbach <dburbach1982@gmail.com>
2019-12-16 21:08:50 -0800

    Add handlers for submit, change, and clear of entered form. Update toUpperCase entered data

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,21 +1,50 @@
 import React, { Component } from "react";
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+        value: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    
+  handleChange(event) {
+    this.setState({ value: ('$' + event.target.value).toUpperCase() });
+  }
+
+  handleSubmit(event) {
+      alert("A name was submitted: " + this.state.value);
+      event.preventDefault();
+  }
+
+  clearSearch = () => {
+      this.setState({ value: "", cursor: 0})
+  }
+
   render() {
     return (
       <div>
         <div> Home </div>
         <form>
           <label>
-            <input type="text" name="search" />
+            $<input
+              type="text"
+              placeholder="Stock Symbol"
+              onChange={this.handleChange}
+              name="search"
+              required
+            />
           </label>
           <input type="submit" value="Submit" />
+          <button value="Clear" onClick={this.clearSearch}>Clear</button>
         </form>
+        <br/>
+        <div> {this.state.value} </div>
       </div>
     );
-    }
+  }
 }
 
 export default Home;
