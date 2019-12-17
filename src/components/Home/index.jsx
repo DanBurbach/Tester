63a8675f7b6fd1e9b6cf1b3f81801d14c94ea443
DanBurbach <dburbach1982@gmail.com>
2019-12-16 23:36:22 -0800

    begin funtion build for change, search, and referencing of returned API data

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,20 +3,28 @@ import React, { Component } from "react";
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-        value: ""
+    this.state = {
+      stock: "",
+      symbolList: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.clearSearch = this.clearSearch.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: ('$' + event.target.value).toUpperCase() });
+    let data = event.target.value.toUpperCase()
+    this.setState({ stock: data });
   }
 
-  handleSubmit(event) {
-      alert("A name was submitted: " + this.state.value);
-      event.preventDefault();
+  handleSubmit = async(event) => {
+    event.preventDefault();
+    var enteredValue = this.state.stock;
+    var url =
+        fetch('https://api.stocktwits.com/api/2/streams/symbol/' + enteredValue + '.json');
+        this.setState({ symbolList : url})
+    //   alert("A name was submitted: " + this.state.value);
+    console.log(this.state.symbolList);
   }
 
   clearSearch = () => {
@@ -37,11 +45,11 @@ class Home extends Component {
               required
             />
           </label>
-          <input type="submit" value="Submit" />
-          <button value="Clear" onClick={this.clearSearch}>Clear</button>
+          <input type="submit" value="Submit" onClick={this.handleSubmit}/>
+          <button value="Clear" onClick={this.clearSearch}>Clear Search List</button>
         </form>
         <br/>
-        <div> {this.state.value} </div>
+        <div> {this.state.stock} </div>
       </div>
     );
   }
