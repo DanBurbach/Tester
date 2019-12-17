f1862221019457b5050f6d9ca13982b28e3c9fda
DanBurbach <dburbach1982@gmail.com>
2019-12-17 09:20:32 -0800

    wrap App with a provider store for Redux puroposes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/App';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>,
 document.getElementById('root'));
 
 serviceWorker.unregister();
