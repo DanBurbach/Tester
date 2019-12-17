079bacefbb66fc0959eac2e4a34693fbf3cdd3cf
DanBurbach <dburbach1982@gmail.com>
2019-12-16 21:07:26 -0800

    initial creation of Results component

---
+++ src/components/Results/index.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <div> Results </div>
+      </div>
+    );
+  }
+}
+
+export default Results;
